1801d7abb37711a95a6b3409424aa9e2d781cc20
Manik Sethi <mksethi@ucdavis.edu>
2024-06-22 18:54:07 -0700

    Initial commit

---
+++ build/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/main.cc.o"
+  "CMakeFiles/raytracer.dir/main.cc.o.d"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+
+    for (int j =0; j<image_height; j++) {
+        std::clog << "\rScanlines remaining:" << (image_height-j) << ' ' << std::flush;
+        for (int i =0; i<image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
+
+            int ir = (255.999 * r);
+            int ig = (255.999 * g);
+            int ib = (255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+
+        }
+    }
+    std::clog << "\rDone.                   \n";
+}
