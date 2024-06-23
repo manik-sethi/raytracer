c58c17c6b3df0230ae6552d72456281d7b7da567
Manik Sethi <mksethi@ucdavis.edu>
2024-06-22 19:08:47 -0700

    added color and ray header files

--- main.cc
+++ main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -7,26 +10,17 @@ int main() {
     int image_width = 256;
     int image_height = 256;
 
-
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-
-    for (int j =0; j<image_height; j++) {
-        std::clog << "\rScanlines remaining:" << (image_height-j) << ' ' << std::flush;
-        for (int i =0; i<image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = (255.999 * r);
-            int ig = (255.999 * g);
-            int ib = (255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
-
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    std::clog << "\rDone.                   \n";
+
+    std::clog << "\rDone.                 \n";
 }
